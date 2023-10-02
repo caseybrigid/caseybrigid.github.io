47b4943f0229ffc9a084f62dafe1487a9d78730e
Barak Binaymin <barak.binyamin555@gmail.com>
2023-10-02 11:25:07 -0400

    Image loading improvements

--- src/src/components/portfolio.vue
+++ src/src/components/portfolio.vue
@@ -1,11 +1,12 @@
 <template>
   <!-- Reference https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_image_grid_responsive -->
+  <!-- Reference https://imagekit.io/blog/lazy-loading-images-complete-guide/ -->
     <div class="grid-container">
       <div v-for="group in images" class="grid-item">
         <Transition v-for="name in group">
           <div v-if="show" class="image-container">
             <img 
-                alt=""
+                alt="" class="lazy"
                 :src="`/portfolio/${name}`"
                 loading="lazy"
               > 
@@ -38,6 +39,36 @@ export default{
       metadata: {},
       show: false
     }
+  },
+  computed(){
+    // Magic lazy loading https://imagekit.io/blog/lazy-loading-images-complete-guide/
+    var lazyloadImages = document.querySelectorAll("img.lazy"); 
+      console.log(lazyloadImages)   
+      var lazyloadThrottleTimeout;
+      
+      function lazyload () {
+        if(lazyloadThrottleTimeout) {
+          clearTimeout(lazyloadThrottleTimeout);
+        }    
+        
+        lazyloadThrottleTimeout = setTimeout(function() {
+            var scrollTop = window.scrollY;
+            lazyloadImages.forEach(function(img) {
+                if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                  img.src = img.dataset.src;
+                  img.classList.remove('lazy');
+                }
+            });
+            if(lazyloadImages.length == 0) { 
+              document.removeEventListener("scroll", lazyload);
+              window.removeEventListener("resize", lazyload);
+              window.removeEventListener("orientationChange", lazyload);
+            }
+        }, 20);
+      }
+      document.addEventListener("scroll", lazyload);
+      window.addEventListener("resize", lazyload);
+      window.addEventListener("orientationChange", lazyload);
   },
   async mounted(){
     // Allow animations to show before requesting all the images and slowing stuff down

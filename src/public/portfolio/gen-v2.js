95c43dea94ac013843a4f54f1ba77beb6ed1a683
Barak Binaymin <barak.binyamin555@gmail.com>
2024-06-04 09:35:28 -0400

    updated scripts

--- src/public/portfolio/gen-v2.js
+++ src/public/portfolio/gen-v2.js
@@ -6,7 +6,7 @@ const files= readdirSync('src/public/portfolio/img')
 
 let thisGroup=1
 let times=1
-for (let i=1; i<files.length; i++){
+for (let i=0; i<files.length; i++){
     if (files[i]!="description.json" && !files[i].includes(".js")){
         if (times>=2){thisGroup=thisGroup%2+1;times=1}else{times++}
         jsonOut.photos.push(
